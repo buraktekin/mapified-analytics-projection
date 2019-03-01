078411a7d53ac96129d2c2909cd277a84d2f1aa4
buraktekin <tknbrk@gmail.com>
2019-03-01 03:17:12 +0300

    🔥Map is integrated🔥

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import GMap, { Map } from './components/GMap';
+import GMap from './components/GMap';
 
 import logo from './logo.svg';
 import './App.css';
-
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-}
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <GMap></GMap>
+        <GMap
+        googleMapURL='https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyCk4cjEaM7VvvpWgS_CutqF1AIg8OEGItI&center=43.601100318465356,26.728627203911365&zoom=5&format=png&maptype=roadmap&style=color:0x03d399%7Cvisibility:on&style=element:geometry%7Ccolor:0xf5f5f5&style=element:labels%7Ccolor:0x25d399%7Csaturation:100&style=element:labels.icon%7Cvisibility:off&style=element:labels.text%7Ccolor:0x25d399%7Cvisibility:simplified&style=element:labels.text.fill%7Ccolor:0x099d72%7Csaturation:100%7Cweight:4.5&style=element:labels.text.stroke%7Ccolor:0xf5f5f5&style=feature:administrative.country%7Celement:geometry%7Ccolor:0x16e188%7Cweight:0.5&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:administrative.locality%7Celement:geometry.fill%7Ccolor:0xff6993&style=feature:administrative.province%7Celement:geometry.stroke%7Cweight:1&style=feature:administrative.province%7Celement:labels.text%7Ccolor:0xf89245&style=feature:landscape%7Celement:geometry.fill%7Ccolor:0xd6d6d6%7Clightness:100&style=feature:landscape.natural.terrain%7Cvisibility:off&style=feature:landscape.natural.terrain%7Celement:geometry.fill%7Ccolor:0xff7271&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0x5ee5b6&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:road%7Celement:geometry%7Ccolor:0xffffff&style=feature:road%7Celement:geometry.fill%7Ccolor:0x00d0c8%7Cweight:0.5&style=feature:road.arterial%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:road.highway%7Csaturation:100%7Cvisibility:off&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:road.local%7Cvisibility:off&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:transit%7Cvisibility:off&style=feature:transit.line%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:transit.station%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:water%7Celement:geometry%7Ccolor:0x455e96&style=feature:water%7Celement:geometry.stroke%7Cweight:4&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&size=480x360'
+        ></GMap>
       </div>
     );
   }
--- src/components/GMap.js
+++ src/components/GMap.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+const styles = require("../assets/theme/gmaps_theme.json");
+const { compose, withProps, withStateHandlers } = require("recompose");
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-}
+// .ENV initialize
+require('dotenv').config('.env');
 
-export class GMap extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat: -1.2884,
-          lng: 36.8233
-        }}
-      />
-    );
-  }
-}
+// Import custom icon for map marker
+// You can use this if you need to support IE11 and lower.
+// const mapMarker = require('./GoogleMapMarker.svg')
 
-export default GoogleApiWrapper({
-  apiKey: 'YOUR_GOOGLE_API_KEY_GOES_HERE'
-})(GMap);
+
+const GMap = 
+  compose(
+    withProps({
+      googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCk4cjEaM7VvvpWgS_CutqF1AIg8OEGItI&v=3.exp&libraries=geometry,drawing,places",
+      loadingElement: <div style={{ height: `100%` }} />,
+      containerElement: <div style={{ height: `100vh` }} />,
+      mapElement: <div style={{ height: `100%` }} />,
+      center: { lat: 39.0096473, lng: 30.6776116 },
+    }),
+    withStateHandlers(() => ({
+      isOpen: false,
+    }), {
+      onToggleOpen: ({ isOpen }) => () => ({
+        isOpen: !isOpen,
+      })
+    }),
+    withScriptjs,
+    withGoogleMap
+  )(props =>
+    <GoogleMap
+      defaultZoom={4}
+      defaultCenter={props.center}
+      defaultOptions={{ styles: styles }}
+    >
+    </GoogleMap>
+  );
+
+export default GMap;
